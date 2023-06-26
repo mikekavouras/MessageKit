b42cc0afab1210afb14bde385b9ef875c5016a4c
Mike Kavouras <kavourasm@gmail.com>
2023-06-25 21:56:30 -0700

    Update upTo version

--- Package.swift
+++ Package.swift
@@ -31,7 +31,7 @@ let package = Package(
         .plugin(name: "SwiftFormatPlugin", targets: ["SwiftFormatPlugin"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/mikekavouras/InputBarAccessoryView", .upToNextMajor(from: "6.2.0")),
+        .package(url: "https://github.com/mikekavouras/InputBarAccessoryView", .upToNextMajor(from: "0.1.0")),
     ],
     targets: [
         // MARK: - MessageKit
